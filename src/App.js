da1a03b59c32d0f6351fe3e2fdf187bf08f3d482
akhil bhumireddy <akhil.bhumireddy99@gmail.com>
2024-04-07 11:30:43 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import Banner from "./components/Banner";
+import Header from "./components/Header";
+import KickOff from "./components/KickOff";
+import Placements from "./components/Placements";
+import WhatWillYouLearn from "./components/WhatWillYouLearn";
+import WhyChooseUs from "./components/WhyChooseUs";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <WhyChooseUs />
+      <WhatWillYouLearn />
+      <Placements />
+      <KickOff />
+    </>
+  );
+}
+
+export default App;
